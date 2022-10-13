ae032eac498754e70677c87e869386ef8cba2181
Justin Chu <justinchuby@users.noreply.github.com>
2022-10-12 17:18:09 -0700

    softmax_loss

--- apps/simple_ml.py
+++ apps/simple_ml.py
@@ -1,14 +1,20 @@
-import struct
 import gzip
-import numpy as np
-
+import struct
 import sys
+
+import numpy as np
+import numpy_typing as npt
+
+import beartype
 
 sys.path.append("python/")
 import needle as ndl
 
 
-def parse_mnist(image_filesname, label_filename):
+@beartype.beartype
+def parse_mnist(
+    image_filesname: str, label_filename: str
+) -> tuple[npt.NDArray[np.float32], npt.NDArray[np.uint8]]:
     """Read an images and labels file in MNIST format.  See this page:
     http://yann.lecun.com/exdb/mnist/ for a description of the file format.
 
@@ -17,7 +23,7 @@ def parse_mnist(image_filesname, label_filename):
         label_filename (str): name of gzipped labels file in MNIST format
 
     Returns:
-        Tuple (X,y):
+        tuple (X,y):
             X (numpy.ndarray[np.float32]): 2D numpy array containing the loaded
                 data.  The dimensionality of the data should be
                 (num_examples x input_dim) where 'input_dim' is the full
@@ -26,16 +32,29 @@ def parse_mnist(image_filesname, label_filename):
                 should be normalized to have a minimum value of 0.0 and a
                 maximum value of 1.0.
 
-            y (numpy.ndarray[dypte=np.int8]): 1D numpy array containing the
-                labels of the examples.  Values should be of type np.int8 and
+            y (numpy.ndarray[dypte=np.uint8]): 1D numpy array containing the
+                labels of the examples.  Values should be of type np.uint8 and
                 for MNIST will contain the values 0-9.
     """
-    ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
-    ### END YOUR SOLUTION
+    # Load images
+    with gzip.open(image_filesname) as f:
+        magic, n_images, rows, cols = struct.unpack(">IIII", f.read(16))
+        print("magic number is", magic)
+        # f already points to the first image
+        image_data = np.frombuffer(f.read(), dtype=np.uint8).reshape(
+            n_images, rows * cols
+        )
+        images = image_data.astype(np.float32) / 255.0
+
+    with gzip.open(label_filename) as f:
+        magic, n_labels = struct.unpack(">II", f.read(8))
+        labels = np.frombuffer(f.read(), dtype=np.uint8)
+
+    return images, labels
 
 
-def softmax_loss(Z, y_one_hot):
+@beartype.beartype
+def softmax_loss(Z: ndl.Tensor, y_one_hot: ndl.Tensor) -> ndl.Tensor:
     """Return softmax loss.  Note that for the purposes of this assignment,
     you don't need to worry about "nicely" scaling the numerical properties
     of the log-sum-exp computation, but can just compute this directly.
@@ -44,16 +63,19 @@ def softmax_loss(Z, y_one_hot):
         Z (ndl.Tensor[np.float32]): 2D Tensor of shape
             (batch_size, num_classes), containing the logit predictions for
             each class.
-        y (ndl.Tensor[np.int8]): 2D Tensor of shape (batch_size, num_classes)
+        y_one_hot (ndl.Tensor[np.int8]): 2D Tensor of shape (batch_size, num_classes)
             containing a 1 at the index of the true label of each example and
             zeros elsewhere.
 
     Returns:
         Average softmax loss over the sample. (ndl.Tensor[np.float32])
     """
-    ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
-    ### END YOUR SOLUTION
+    # Take exp of the input
+    exps = ndl.exp(Z)
+    loss = ndl.log(ndl.summation(exps, axis=1)) - ndl.summation(Z * y_one_hot, axis=1)
+    # TODO: check if this is the right way of getting the shape
+    return ndl.summation(loss) / np.prod(loss.shape)
+
 
 
 def nn_epoch(X, y, W1, W2, lr=0.1, batch=100):
